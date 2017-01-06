3b6d5a12a5a5cc728948183fb470f62540d04655
Noah Berman <noah@noahberman.org>
2017-01-06 11:50:54 -0700

    move search logic to service

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,29 +5,6 @@ class StoresController < ApplicationController
   
   def index
     @zipcode = params["zipcode"]
-    @all_stores = send_store_request(@zipcode)
-    binding.pry
-  end
-  
-  
-  private
-  
-  def conn
-    Faraday.new(:url => "https://api.bestbuy.com") do |faraday|
-      faraday.request :url_encoded
-      faraday.adapter Faraday.default_adapter
-    end
-  end
-  
-  def send_store_request(zipcode)
-    response = conn.get "/v1/stores", {   :zipcode => "#{zipcode}",
-                                          :area => 25,
-                                          :format => "json", 
-                                          :show => "storeId,storeType,longName,city,distance,phone,storeType,region",
-                                          :apiKey => ENV['BEST_BUY_API_KEY']}
-    JSON.parse(response.body)
-  end
-  
-  
-  
+    @all_stores = StoreService.send_store_request(@zipcode)
+  end  
 end
--- spec/features/stores/as_a_user_I_should_see_list_of_stores_spec.rb
+++ spec/features/stores/as_a_user_I_should_see_list_of_stores_spec.rb
@@ -36,9 +36,17 @@ describe "list of Best Buy stores" do
       expect(current_path).to include("/search")
       expect(page).to have_content("10 Total Stores")
     end
+
+    xscenario "after searching I should exactly the right number of stores", :vcr do
+      visit "/"
+      
+      fill_in "_search_zipcode", with: 80202
+      click_on "Search for nearest store"
+      
+      expect(current_path).to include("/search")
+      expect(page).to have_content("10 Total Stores")
+    end
   end
 end
 
-  # And I should see a message that says "16 Total Stores"
-  # And I should see exactly 15 results
   # And I should see the long name, city, distance, phone number and store type for each of the 15 results
