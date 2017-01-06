bf5bd055bae362e31930ff7660927bdd2571416b
Noah Berman <noah@noahberman.org>
2017-01-06 09:37:54 -0700

    fabricator installed

--- spec/requests/api/v1/get_request_for_all_items_spec.rb
+++ spec/requests/api/v1/get_request_for_all_items_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-
 describe 'items endpoint' do
   context "GET /api/v1/items" do
     scenario "I receive a 200 JSON response" do
+      store = Fabricate.times(10, :item)
       get "/api/v1/items"
       
       items = JSON.parse(response.body)
       
       expect(response).to be_success
-      binding.pry
+
     end
   end
 end
