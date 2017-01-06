3c46f5afdb8b74af96fccc559c0adf55b423e1c5
Noah Berman <noah@noahberman.org>
2017-01-06 11:38:52 -0700

    quasi-functional API call

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,26 +1,29 @@
 class StoresController < ApplicationController
   def search
-    redirect_to "/search"
+    redirect_to search_path(zipcode: params["/search"]["zipcode"])
   end
   
   def index
-    
+    send_store_request(params["zipcode"])
   end
   
   
   private
   
-  self.conn
+  def conn
     Faraday.new(:url => "https://api.bestbuy.com") do |faraday|
       faraday.request :url_encoded
-      faraay.adapter Faraday.default_adapter
+      faraday.adapter Faraday.default_adapter
     end
   end
   
-  def self.send_store_request(zipcode)
-    response = conn.get "/v1/stores(area('#{zipcode}', 25))", { :format => "json", 
-                                                                :show => "storeId,storeType,longName,city,distance,phone,storeType,region",
-                                                                :apiKey => ENV['BEST_BUY_API_KEY']}
+  def send_store_request(zipcode)
+    response = conn.get "/v1/stores", {   :zipcode => "#{zipcode}",
+                                          :format => "json", 
+                                          :show => "storeId,storeType,longName,city,distance,phone,storeType,region",
+                                          :apiKey => ENV['BEST_BUY_API_KEY']}
+    binding.pry
+    JSON.parse(response.body)
   end
   
   
