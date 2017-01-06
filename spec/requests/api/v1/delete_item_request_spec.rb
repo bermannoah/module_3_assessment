cb71a9d81467c92c6673f1afa26f79fee2ea135a
Noah Berman <noah@noahberman.org>
2017-01-06 09:58:51 -0700

    first test for DELETE action

--- spec/requests/api/v1/delete_item_request_spec.rb
+++ spec/requests/api/v1/delete_item_request_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 describe 'items endpoint' do
   context "GET /api/v1/items" do
-    scenario "I receive a 200 JSON response" do
+    scenario "I receive a 204 JSON response" do
       store = Fabricate.times(10, :item)
-      item_id = store.first.id
     
-      get "/api/v1/items/#{item_id}"
+      delete "/api/v1/items/1"
       
-      item = JSON.parse(response.body)
+      result = JSON.parse(response.body)
 
-      expect(response).to be_success
-      expect(item).to be_a(Hash)
+      expect(response).to eq(204)
     end
 
-    scenario "I receive the right item" do
+    xscenario "I receive the right item" do
       store = Fabricate.times(10, :item)
       item_id = store.first.id
 
@@ -27,7 +25,7 @@ describe 'items endpoint' do
       expect(item["id"]).to eq(item_id)
     end
 
-    scenario "I do NOT see the created at and updated at times" do
+    xscenario "I do NOT see the created at and updated at times" do
       store = Fabricate.times(10, :item)
       item_id = store.first.id
 
@@ -41,3 +39,6 @@ describe 'items endpoint' do
     end
   end
 end
+
+# when I send a DELETE request to /api/v1/items/1'
+# I receive a 204 JSON response if the record is successfully deleted
