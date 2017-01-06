8c6d5c5bfdccf7dc783f09d428b992f86587cd8a
Noah Berman <noah@noahberman.org>
2017-01-06 10:03:12 -0700

    delete action is tested and working

--- spec/requests/api/v1/delete_item_request_spec.rb
+++ spec/requests/api/v1/delete_item_request_spec.rb
@@ -10,33 +10,15 @@ describe 'items endpoint' do
       expect(response.status).to eq(204)
     end
 
-    xscenario "I receive the right item" do
+    scenario "I delete the right item" do
       store = Fabricate.times(10, :item)
       item_id = store.first.id
+      expect(Item.count).to eq(10)
 
-      get "/api/v1/items/#{item_id}"
+      delete "/api/v1/items/#{item_id}"
       
-      item = JSON.parse(response.body)
-
-      expect(response).to be_success
-      expect(item["name"]).to eq(store.first.name)
-      expect(item["id"]).to eq(item_id)
-    end
-
-    xscenario "I do NOT see the created at and updated at times" do
-      store = Fabricate.times(10, :item)
-      item_id = store.first.id
-
-      get "/api/v1/items/#{item_id}"
-      
-      item = JSON.parse(response.body)
-
-      expect(response).to be_success
-      expect(item).to_not include("created_at")
-      expect(item).to_not include("updated_at")
+      expect(response.status).to eq(204)
+      expect(Item.count).to eq(9)
     end
   end
 end
-
-# when I send a DELETE request to /api/v1/items/1'
-# I receive a 204 JSON response if the record is successfully deleted
