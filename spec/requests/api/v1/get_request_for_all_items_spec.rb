4d6401bb1a228ecd2df87e9fcf45c9c2b994da8e
Noah Berman <noah@noahberman.org>
2017-01-06 09:48:27 -0700

    test for item show route

--- spec/requests/api/v1/get_request_for_all_items_spec.rb
+++ spec/requests/api/v1/get_request_for_all_items_spec.rb
@@ -38,6 +38,3 @@ describe 'items endpoint' do
     end
   end
 end
-# When I send a GET request to /api/v1/items
-# I receive a 200 JSON response containing all items
-# And each item has an id, name, description, and image_url but NOT the created or updated at
---
+++ spec/requests/api/v1/get_request_for_one_item_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'items endpoint' do
+  context "GET /api/v1/items" do
+    scenario "I receive a 200 JSON response" do
+      store = Fabricate.times(10, :item)
+      get "/api/v1/items/1"
+      
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item).to be_a(Hash)
+    end
+
+    scenario "I receive the right item" do
+      store = Fabricate.times(10, :item)
+      get "/api/v1/items/1"
+      
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["name"]).to eq(store.first.name)
+      expect(item.count).to eq(1)
+    end
+
+    scenario "I do NOT see the created at and updated at times" do
+      store = Fabricate.times(10, :item)
+      get "/api/v1/items/1"
+      
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item).to_not include("created_at")
+      expect(item).to_not include("updated_at")
+    end
+  end
+end
+
+# when I send a GET request to /api/v1/items/1'
+# I get just one item (that item) back with rel info
