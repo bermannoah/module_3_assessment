88677213a75346aacf726005e4abadd6895c2560
Noah Berman <noah@noahberman.org>
2017-01-09 19:01:28 -0700

    change to PORO from table for stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,10 @@
 class StoresController < ApplicationController
   def search
     redirect_to search_path(zipcode: params["/search"]["zipcode"])
+    zipcode = params["zipcode"]
+    @all_stores = Store.store_process(zipcode)
   end
   
   def index
-    zipcode = params["zipcode"]
-    @all_stores = Store.store_process(zipcode)
-    # @all_stores = Store.where(search_zip: zipcode)
   end  
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,17 +1,16 @@
 class Store
-  def initialize(raw_stores)
-    @long_name = store["longName"]
+  def initialize(raw_store)
+    @long_name = raw_store["longName"]
+    @city = raw_store["city"]
+    @distance = raw_store["distance"]
+    @phone_number = raw_store["phone"]
+    @store_type = raw_store["storeType"]
   end
   
   def self.store_process(zipcode)
     all_stores = StoreService.send_store_request(zipcode)
     all_stores.each do |store|
-      Store.new(long_name: store["longName"],
-                   city: store["city"],
-                   distance: store["distance"],
-                   phone_number: store["phone"],
-                   store_type: store["storeType"],
-                   search_zip: zipcode)
+      Store.new(store)
     end
   end
     
