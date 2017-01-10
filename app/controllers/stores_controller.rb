3b83c221240d80c174c737e9dbb8d9d8dc997e31
Noah Berman <noah@noahberman.org>
2017-01-09 19:05:03 -0700

    simplify store controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,10 +1,6 @@
 class StoresController < ApplicationController
   def search
-    redirect_to search_path(zipcode: params["/search"]["zipcode"])
-    zipcode = params["zipcode"]
+    zipcode = params["/search"]["zipcode"]
     @all_stores = Store.store_process(zipcode)
   end
-  
-  def index
-  end  
 end
